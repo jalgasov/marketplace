26e0c0f0ad19dffcd2f4dc4692324d764ad1e487
Jalgas <grigorigrigorievich@gmail.com>
2025-10-05 23:42:14 +0300

    testapp migrated

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fortest', # Added the fortest app here    
 ]
 
 MIDDLEWARE = [
